c1c3ecb5081f56c276e6a89c63bc16e19b1df14d
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-04-05 18:35:02 +0200

    solution

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
-// Tu código aquí
+Vue.createApp({
+    data() {
+        return {
+            newMessage: "",
+            lastMessage: "Hello world",
+            errorVisible: false
+        }
+    },
 
+    methods: {
+        sendMessage() {
+            if (!this.isNewMessageValid()) {
+                this.showErrorMessage();
+                return;
+            }
 
-form.addEventListener('submit', function(e){
-    console.log("Hemos hecho clic en el botón de submit!")
+            this.lastMessage = this.newMessage;
+            this.newMessage = "";
+        },
 
-})
+        isNewMessageValid() {
+            return (this.newMessage.trim().length > 0) ? true : false;
+        },
+
+        showErrorMessage() {
+            this.errorVisible = true;
+            setTimeout(() => this.errorVisible = false, 3000);
+        }
+    }
+}).mount("#app");
